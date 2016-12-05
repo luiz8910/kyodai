2619433ee7071ab81f4a23fb3b45687fa0725d14
steve <support@h2ocontabilidade.com.br>
2016-12-05 15:55:35 -0200

    commit julio

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -30,164 +30,119 @@
                 <div class="divisor-default"></div>
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <ul class="list-inline">
-                            <li class="linha-header-index hidden-xs"></li>
-                            <li class=""><h2 class="text-color-1">Culinária Japonesa</h2></li>
-                            <li class="linha-header-index hidden-sm hidden-md"></li>
+                            <li class=""><h1><div style="height: 10px !important; margin-top: -45px !important; display: block !important; position: absolute !important; left: 310px;">_____</div></h1></li>
+                            <li class=""><h2 class="text-color-1" style="">Culinária Japonesa </h2></li>
+                            <li class=""><h1><div style="height: 10px !important; margin-top: -45px !important; display: block !important; position: absolute !important; ">_____</div></h1></li>
                         </ul>
                     </div>
             </div>
-
-            <div class="row text-center text-title-description">
-                <h4>Bem Vindo ao Kyodai - Sorocaba</h4>
-            </div>
-
-            <div class="row text-center text-title-description">
-                <h4 class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
-                    Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
-                    Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
-                    Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
-                </h4>
-            </div>
+            <div class="row ">
+                <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-center text-title-description">
+                    <h4>Bem Vindo ao Kyodai - Sorocaba</h4><br>
+                    <p class="">
+                        Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
+                        Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
+                        Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
+                    </p>
+                </div>
+            </div> <!-- ./ row -->
         </div>
     </section>
 
     <section class="bg-tripadvisor section-padding">
-
-        <div class="container col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="row col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <br><br>
-                <ul class="list-inline text-center">
-                    <li class="linha-header-index-menu hidden-xs"></li>
-                    <li><h3 class="banner-menu-h3">Avaliações no TripAdvisor</h3></li>
-                    <li class="linha-header-index-menu hidden-sm"></li>
-                </ul>
-                <br><br>
-            </div>
-
-            <div class="row col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div id="TA_selfserveprop896" class="TA_selfserveprop box-tripadvisor-avaliacoes">
-                    <ul id="YJDAhtiY" class="TA_links izOJEYcK">
-                        <li id="RFYUIh6dPUM" class="Yq6114VgptCQ">
-                            <a target="_blank" href="https://www.tripadvisor.com.br/">
-                                <img src="https://www.tripadvisor.com.br/img/cdsi/img2/branding/150_logo-11900-2.png" alt="TripAdvisor"/>
-                            </a>
-                        </li>
+        <div class="container">
+           <div class="row text-center">
+                <div class="divisor-default"></div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <ul class="list-inline">
+                        <li class="banner-menu-h3"><h1><div style="height: 10px !important; margin-top: -45px !important; display: block !important; position: absolute !important; left: 299px;">_____</div></h1></li>
+                        <li class=""><h2 class="banner-menu-h3" style="">Avaliações no TripAdvisor</h2></li>
+                        <li class="banner-menu-h3"><h1><div style="height: 10px !important; margin-top: -45px !important; display: block !important; position: absolute !important; ">_____</div></h1></li>
                     </ul>
                 </div>
-                <script src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=896&amp;locationId=4525168&amp;lang=pt&amp;rating=true&amp;nreviews=4&amp;writereviewlink=true&amp;popIdx=true&amp;iswide=true&amp;border=true&amp;display_version=2"></script>
+            </div><br><br><br>
 
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div id="TA_selfserveprop896" class="TA_selfserveprop box-tripadvisor-avaliacoes col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <ul id="YJDAhtiY" class="TA_links izOJEYcK">
+                            <li id="RFYUIh6dPUM" class="Yq6114VgptCQ">
+                                <a target="_blank" href="https://www.tripadvisor.com.br/">
+                                    <img src="https://www.tripadvisor.com.br/img/cdsi/img2/branding/150_logo-11900-2.png" alt="TripAdvisor"/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <script src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=896&amp;locationId=4525168&amp;lang=pt&amp;rating=true&amp;nreviews=4&amp;writereviewlink=true&amp;popIdx=true&amp;iswide=true&amp;border=true&amp;display_version=2"></script>
+                </div> <!-- ./ col-lg-6 col-md-6 col-sm-12 col-xs-12 -->
 
-                <div id="TA_certificateOfExcellence424" class="TA_certificateOfExcellence box-tripadvisor">
-                    <ul id="TVvDp0089djC" class="TA_links gQLf98KHM5V">
-                        <li id="0FMJeV" class="PtD8o6KKRN">
-                            <a target="_blank" href="https://www.tripadvisor.com.br/Restaurant_Review-g303632-d4525168-Reviews-Restaurante_Japones_Kyodai-Sorocaba_State_of_Sao_Paulo.html">
-                                <img src="https://www.tripadvisor.com.br/img/cdsi/img2/awards/CoE2016_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <script src="https://www.jscache.com/wejs?wtype=certificateOfExcellence&amp;uniq=424&amp;locationId=4525168&amp;lang=pt&amp;year=2016&amp;display_version=2">
-                </script>
-
-                <div id="TA_restaurantWidgetGreen202" class="TA_restaurantWidgetGreen box-tripadvisor-recomendacoes">
-                    <ul id="IqIFIIXNE" class="TA_links fxBtMQBa">
-                        <li id="z6tXRxihUGk" class="5Sh4t8">
-                            <a target="_blank" href="https://www.tripadvisor.com.br/"><img src="https://www.tripadvisor.com.br/img/cdsi/partner/tripadvisor_logo_117x18-24177-2.png" alt="TripAdvisor"/></a>
-                        </li>
-                    </ul>
-                </div>
-                <script src="https://www.jscache.com/wejs?wtype=restaurantWidgetGreen&amp;uniq=202&amp;locationId=4525168&amp;icon=knifeAndFork&amp;lang=pt&amp;display_version=2"></script>
-
-            </div>
-        </div>
-
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                    <div id="TA_certificateOfExcellence424" class="TA_certificateOfExcellence box-tripadvisor">
+                        <ul id="TVvDp0089djC" class="TA_links gQLf98KHM5V">
+                            <li id="0FMJeV" class="PtD8o6KKRN">
+                                <a target="_blank" href="https://www.tripadvisor.com.br/Restaurant_Review-g303632-d4525168-Reviews-Restaurante_Japones_Kyodai-Sorocaba_State_of_Sao_Paulo.html">
+                                    <img src="https://www.tripadvisor.com.br/img/cdsi/img2/awards/CoE2016_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <script src="https://www.jscache.com/wejs?wtype=certificateOfExcellence&amp;uniq=424&amp;locationId=4525168&amp;lang=pt&amp;year=2016&amp;display_version=2">
+                    </script>
+                </div><!-- ./ col-lg-6 col-md-6 col-sm-12 col-xs-12 -->
+            </div> <!-- /. row -->
+        </div> <!-- /. container -->
     </section>
 
 
     <section class="bg-section-index-menu section-padding">
         <div class="container">
+           <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12 text-color-white">
+                    <h2 class="text-center "><i class="linha-1">_____</i> Destaque da Casa <i class="linha-1">_____</i></h2><br>
+                </div>
+            </div><br>
+
             <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <br><br>
-                    <ul class="list-inline text-center">
-                        <li class="linha-header-index-menu hidden-xs"></li>
-                        <li><h3 class="banner-menu-h3">Destaques da Casa</h3></li>
-                        <li class="linha-header-index-menu hidden-sm"></li>
-                    </ul>
-                    <br>
-                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12 ajuste-box-cardapio">
+                    <img src="site/assets/img/home/destaque-1.png" class="img-responsive img-circle img-cardapio-ajuste centered" alt="Imagem responsiva"><br>
+                    <h4 class="text-center text-uppercase text-color-white">Bolinho Japonés</h4>
+                    <p class="text-justify text-color-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus aspernatur hic tempore, soluta quia nam ullam atque odit. </p>
+                </div> <!-- div /.col-md-3 .col-sm-6 .col-xs-12 -->
 
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6">
-                    <img src="site/assets/img/home/destaque-1.png"
-                         class="centered img-circle img-responsive banner-menu-img col-lg-6 col-md-3 col-xs-12 col-sm-6">
+                <div class="col-md-3 col-sm-6 col-xs-12 ajuste-box-cardapio">
+                    <img src="site/assets/img/home/destaque-1.png" class="img-responsive img-circle img-cardapio-ajuste centered" alt="Imagem responsiva"><br>
+                    <h4 class="text-center text-uppercase text-color-white">Bolinho Japonés</h4>
+                    <p class="text-justify text-color-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus aspernatur hic tempore, soluta quia nam ullam atque odit. </p>
+                </div> <!-- div /.col-md-3 .col-sm-6 .col-xs-12 -->
 
-                    <h3 class="banner-menu-h3 col-lg-12 col-sm-12 col-xs-12">
-                        Bolinho Japonês
-                    </h3>
+                <div class="col-md-3 col-sm-6 col-xs-12 ajuste-box-cardapio">
+                    <img src="site/assets/img/home/destaque-1.png" class="img-responsive img-circle img-cardapio-ajuste centered" alt="Imagem responsiva"><br>
+                    <h4 class="text-center text-uppercase text-color-white">Bolinho Japonés</h4>
+                    <p class="text-justify text-color-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus aspernatur hic tempore, soluta quia nam ullam atque odit. </p>
+                </div> <!-- div /.col-md-3 .col-sm-6 .col-xs-12 -->
 
-                    <h4 class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-menu-index">
-                        Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
-                    </h4>
+                <div class="col-md-3 col-sm-6 col-xs-12 ajuste-box-cardapio">
+                    <img src="site/assets/img/home/destaque-1.png" class="img-responsive img-circle img-cardapio-ajuste centered" alt="Imagem responsiva"><br>
+                    <h4 class="text-center text-uppercase text-color-white">Bolinho Japonés</h4>
+                    <p class="text-justify  text-color-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus aspernatur hic tempore, soluta quia nam ullam atque odit. </p>
+                </div> <!-- div /.col-md-3 .col-sm-6 .col-xs-12 -->
 
-                </div>
 
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6">
-                    <img src="site/assets/img/home/destaque-1.png"
-                         class="centered img-circle img-responsive banner-menu-img col-lg-6 col-md-3 col-xs-12 col-sm-6">
 
-                    <h3 class="banner-menu-h3 col-lg-12 col-sm-12 col-xs-12">
-                        Bolinho Japonês
-                    </h3>
-
-                    <h4 class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-menu-index">
-                        Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
-                    </h4>
-
-                </div>
-
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6">
-                    <img src="site/assets/img/home/destaque-1.png"
-                         class="centered img-circle img-responsive banner-menu-img col-lg-6 col-md-3 col-xs-12 col-sm-6">
-
-                    <h3 class="banner-menu-h3 col-lg-12 col-sm-12 col-xs-12">
-                        Bolinho Japonês
-                    </h3>
-
-                    <h4 class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-menu-index">
-                        Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
-                    </h4>
-
-                </div>
-
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6">
-                    <img src="site/assets/img/home/destaque-1.png"
-                         class="centered img-circle img-responsive banner-menu-img col-lg-6 col-md-3 col-xs-12 col-sm-6">
-
-                    <h3 class="banner-menu-h3 col-lg-12 col-sm-12 col-xs-12">
-                        Bolinho Japonês
-                    </h3>
-
-                    <h4 class="col-md-12 col-lg-12 col-sm-12 col-xs-12 text-menu-index">
-                        Fundado em 1993, o Kyodai Sorocaba é o primeiro restaurante japonês tradicional de sorocaba
-                    </h4>
-
-                </div>
 
             </div> <!-- div row -->
         </div> <!-- div container -->
     </section>
 
     <section class="bg-section-index-horario section-padding">
-
         <div class="container">
             <div class="row">
-                <ul class="list-inline text-center">
-                    <li class="linha-header-index-menu-black hidden-xs hidden-sm"></li>
-                    <li><h3 class="banner-menu-h3-black">Horário de Funcionamento</h3></li>
-                    <li class="linha-header-index-menu-black hidden-xs hidden-sm"></li>
-                </ul>
+                <div class="col-md-12 col-sm-12 col-xs-12 text-color-1">
+                    <h2 class="text-center "><i class="linha-1">_____</i> Horários de Funcionamento <i class="linha-1">_____</i></h2><br>
+                </div>
+            </div><br>
 
-
+            <div class="row">
                 <div class="jumbotron">
                     <div class="text-left">Segunda a Sexta:</div>
                     <div class="text-right jumbotron-content">18:00 ás 22:00h</div>
@@ -199,20 +154,20 @@
                     <div class="text-right jumbotron-content">18:00 ás 22:00h</div>
                     <br>
                 </div> <!-- div jumbotron -->
+            </div> <!-- div row -->
 
-                <ul class="list-inline text-center">
-                    <li class="linha-header-index-menu-black hidden-xs hidden-sm"></li>
-                    <li><h3 class="banner-menu-h3-black">Horário de Delivery</h3></li>
-                    <li class="linha-header-index-menu-black hidden-xs hidden-sm"></li>
-                </ul>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12 text-color-1">
+                    <h2 class="text-center "><i class="linha-1">_____</i> Horários de Delivery   <i class="linha-1">_____</i></h2><br>
+                </div>
+            </div><br>
 
-
+            <div class="row">
                 <div class="jumbotron">
                     <div class="text-left">Segunda a Sexta:</div>
                     <div class="text-right jumbotron-content">18:00 ás 22:00h</div>
                     <br>
                 </div> <!-- div jumbotron -->
-
             </div> <!-- div row -->
         </div> <!-- div container -->
 
@@ -221,27 +176,17 @@
     <section class="bg-section-index-carousel section-padding">
         <div class="container">
             <div class="row">
-
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <br><br>
-                    <ul class="list-inline text-center">
-                        <li class="linha-header-index-menu hidden-xs"></li>
-                        <li><h3 class="banner-menu-h3">Saiba mais sobre o prato</h3></li>
-                        <li class="linha-header-index-menu hidden-sm"></li>
-                    </ul>
-                    <br>
+                <div class="col-md-12 col-sm-12 col-xs-12 text-color-white">
+                    <h2 class="text-center "><i class="linha-1">_____</i> Saiba mais sobre o prato <i class="linha-1">_____</i></h2><br>
                 </div>
+            </div><br>
 
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <img src="site/assets/img/home/destaque-1.png"
-                         class="centered img-circle img-responsive carousel-img col-lg-offset-1">
-
-                    <div class="carousel-text col-lg-offset-3 col-md-offset-3 col-sm-offset-3 col-xs-offset-3">
-                        <h4 class="banner-menu-h3 text-left ">Bolinho Japonês</h4>
-                        <h5 class="banner-menu-h3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab, dignissimos eligendi eveniet excepturi facilis,
-                        iusto natus necessitatibus obcaecati officia pariatur quae, quas rerum saepe soluta tenetur unde velit voluptatem?</h5>
-                    </div>
-               </div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12 ajuste-box-cardapio text-center">
+                    <img src="site/assets/img/home/destaque-1.png" class="img-responsive img-circle img-cardapio-ajuste centered" alt="Imagem responsiva"><br>
+                    <h4 class="text-center text-uppercase text-color-white">Bolinho Japonés</h4>
+                    <p class="text-center text-color-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus aspernatur hic tempore, soluta quia nam ullam atque odit. </p>
+                </div> <!-- div /.col-md-3 .col-sm-6 .col-xs-12 -->
 
             </div> <!-- div row -->
         </div> <!-- div container -->
